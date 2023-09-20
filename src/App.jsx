58df899406450682cb349fddd9d9f9c456692b62
Kennedy Rodrigues <kennedy.rodrigues1104@gmail.com>
2023-09-20 17:46:04 -0300

    React project finished. Now rest only alterations in css styles and mobile versions. ALL JS CODE FINISHED.

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,92 @@ import { useState } from 'react'
 import "./App.css"
 import Todo from "./components/Todo";
 import Todoform from "./components/Todoform";
+import Search from './components/search';
+import Filter from './components/filter';
 
 function App() {
   const [todos, setTodos] = useState([
     {
-      id:1,
+      id: 1,
       text: "criar funcionalidade x no sistema",
       category: "Trabalho",
       isCompleted: false,
     },
+    
     {
-      id:2,
+      id: 2,
       text: "Ir pra academia",
       category: "Pessoal",
       isCompleted: false,
     },
+    
     {
-      id:3,
+      id: 3,
       text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
     }
+
   ]);
+
+  const [search, setSearch] = useState("");
+  
+  const [filter, setFilter] = useState("all");
+  const [sort, setSort] = useState("asc");
+  
+  const addTodo = (text, category) =>{
+
+    const newTodos = [...todos, {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? 
+      todo : null );
+      setTodos(filteredTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo
+    );
+    setTodos(newTodos);
+  }
 
   return <div className="app">
     <h1>Lista de Tarefas</h1>
+    
+    <Search search={search} setSearch={setSearch} />
+    <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
+    
     <div className="todo-list">
-      {todos.map((todo) => (
-        <Todo key={todos.id} todo={todo}/>
+      {todos
+      .filter((todo) => filter === "All" 
+      ? true : 
+      filter === "Completed" 
+      ? todo.isCompleted 
+      : !todo.isCompleted
+      )
+      
+      .filter((todo) => 
+        todo.text.toLowerCase().includes(search.toLowerCase())
+        )
+        .sort((a, b) => sort === "Asc" 
+        ? a.text.localeCompare(b.text) 
+        : b.text.localeCompare(a.text))
+        .map((todo) => (
+        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
       ))}
     </div>
-    <Todoform />
+    <Todoform addTodo={addTodo}/>
   </div>;
 }
 
--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,31 +1,29 @@
 import { useState } from 'react'
 
-const todoform = () => {
+const todoform = ({ addTodo }) => {
     const [value, setValue] = useState("");
     const [category, setCategory] = useState("");
 
     const handleSubmit = (e) => {
-        if (!value || !category) return;
-        e.preventDefault()
-        // add todo
-        // clean fields
+        e.preventDefault();
+        if (!value || !category) return;       
+        addTodo(value, category);
         setValue("");
         setCategory("");
-
-    }
+    };
 
   return (
     <div className='todo-form'>
       <h2>Criar tarefa:</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Digite o título' onChange={(e) => setValue (e.target.value)} />
+        <input type="text" placeholder='Digite o título' onChange={(e) => setValue(e.target.value)} />
         <select value={category} onChange={(e) => setCategory(e.target.value)}>
             <option value="">Selecione uma categoria</option>
-            <option value="">Trabalho</option>
-            <option value="">Pessoal</option>
-            <option value="">Estudos</option>
+            <option value="Trabalho">Trabalho</option>
+            <option value="Pessoal">Pessoal</option>
+            <option value="Estudos">Estudos</option>
         </select>
-            <button type='submit'>Criar tarefas</button>
+            <button className='create' type='submit'>Criar tarefas</button>
       </form>
     </div>
   )
